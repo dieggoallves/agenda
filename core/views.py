fefe1a4e525e225ad3380854839457cfa2787b6f
Diego Alves <diegodaccmd@yahoo.com.br>
2020-02-12 22:13:24 -0300

    Criado tela de listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+
+#'''Direciona para a agenda ao não especificar no link da pagina'''
+#def index(request):
+#    return redirect('/agenda/')
+
+'''Busca o usuario da sessão e retorna todos os registros desse agente'''
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
